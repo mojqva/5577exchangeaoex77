871df7ab8b5d9213c93edf192280d8a050c8ce3c
Olga <majankagreen@gmail.com>
2022-04-24 12:42:49 +0300

    added pathname check to menu links

--- client/src/components/Navbar/MobileNavigation.js
+++ client/src/components/Navbar/MobileNavigation.js
@@ -21,31 +21,31 @@ const MobileNavigation = ({open, toggleNav}) => {
                     </div>
                     <ul onClick={toggleNav}>
                         <li>
-                            <Link to={'/ru/'} className={location.pathname === '/ru/' ? s.active : null}>Главная</Link>
+                            <Link to={'/ru/'} className={location.pathname === '/ru/' || location.pathname === '/' ? s.active : null}>Главная</Link>
                         </li> 
                         <li>
-                            <Link to={'/ru/booster'} className={location.pathname === '/ru/booster' ? s.active : null}>Скидки</Link>
+                            <Link to={'/ru/booster'} className={location.pathname === '/ru/booster' || location.pathname === '/booster' ? s.active : null}>Скидки</Link>
                         </li> 
                         <li>
-                            <Link to={'/ru/blog'} className={location.pathname === '/ru/blog' ? s.active : null}>Блог</Link>
+                            <Link to={'/ru/blog'} className={location.pathname === '/ru/blog' || location.pathname === '/blog' ? s.active : null}>Блог</Link>
                         </li> 
                         <li>
-                            <Link to={'/ru/about'} className={location.pathname === '/ru/about' ? s.active : null}>О сервисе</Link>
+                            <Link to={'/ru/about'} className={location.pathname === '/ru/about' || location.pathname === '/about' ? s.active : null}>О сервисе</Link>
                         </li> 
                         <li>
-                            <Link to={'/ru/pages/help'} className={location.pathname === '/ru/pages/help' ? s.active : null}>Помощь</Link>
+                            <Link to={'/ru/pages/help'} className={location.pathname === '/ru/pages/help' || location.pathname === '/pages/help' ? s.active : null}>Помощь</Link>
                         </li> 
                         <li>
-                            <Link to={'/ru/reviews'} className={location.pathname === '/ru/reviews' ? s.active : null}>Отзывы</Link>
+                            <Link to={'/ru/reviews'} className={location.pathname === '/ru/reviews' || location.pathname === '/reviews' ? s.active : null}>Отзывы</Link>
                         </li> 
                         <li>
-                            <Link to={'/ru/partners'} className={location.pathname === '/ru/partners' ? s.active : null}>Партнерам</Link>
+                            <Link to={'/ru/partners'} className={location.pathname === '/ru/partners' || location.pathname === '/partners' ? s.active : null}>Партнерам</Link>
                         </li> 
                         <li>
-                            <Link to={'/ru/pages/crypto-helper'} className={location.pathname === '/ru/pages/crypto-helper' ? s.active : null}>Крипто-помощник</Link>
+                            <Link to={'/ru/pages/crypto-helper'} className={location.pathname === '/ru/pages/crypto-helper' || location.pathname === '/pages/crypto-helper' ? s.active : null}>Крипто-помощник</Link>
                         </li> 
                         <li className={s.last}>
-                            <Link  to={'/ru/support'} className={location.pathname === '/ru/support' ? s.active : null}>Тех.поддержка</Link>
+                            <Link  to={'/ru/support'} className={location.pathname === '/ru/support' || location.pathname === '/support' ? s.active : null}>Тех.поддержка</Link>
                         </li> 
                     </ul>
                 </nav>
--- client/src/components/Navbar/NavLinks.js
+++ client/src/components/Navbar/NavLinks.js
@@ -10,33 +10,33 @@ const NavLinks = () => {
         <ul className={s.ul}>
             <div className={s.common}>
                 <li className={s.li}>
-                    <Link to={'/ru/'} className={location.pathname === '/ru/' ? s.active : null}>Главная</Link>
+                    <Link to={'/ru/'} className={location.pathname === '/ru/' || location.pathname === '/' ? s.active : null}>Главная</Link>
                 </li> 
                 <li className={cn(s.li, s.toNone)}>
-                    <Link to={'/ru/booster'} className={location.pathname === '/ru/booster' ? s.active : null}>Скидки</Link>
+                    <Link to={'/ru/booster'} className={location.pathname === '/ru/booster' || location.pathname === '/booster' ? s.active : null}>Скидки</Link>
                 </li> 
                 <li className={cn(s.li, s.toNone)}>
-                    <Link to={'/ru/blog'} className={location.pathname === '/ru/blog' ? s.active : null}>Блог</Link>
+                    <Link to={'/ru/blog'} className={location.pathname === '/ru/blog' || location.pathname === '/blog' ? s.active : null}>Блог</Link>
                 </li>
                 <li className={s.li}>
-                    <Link to={'/ru/about'} className={location.pathname === '/ru/about' ? s.active : null}>О сервисе</Link>
+                    <Link to={'/ru/about'} className={location.pathname === '/ru/about' || location.pathname === '/about' ? s.active : null}>О сервисе</Link>
                 </li> 
                 <li className={s.li}>
-                    <Link to={'/ru/pages/help'} className={location.pathname === '/ru/pages/help' ? s.active : null}>Помощь</Link>
+                    <Link to={'/ru/pages/help'} className={location.pathname === '/ru/pages/help' || location.pathname === '/pages/help' ? s.active : null}>Помощь</Link>
                 </li> 
                 <li className={cn(s.li, s.toNone)}>
-                    <Link to={'/ru/reviews'} className={location.pathname === '/ru/reviews' ? s.active : null}>Отзывы</Link>
+                    <Link to={'/ru/reviews'} className={location.pathname === '/ru/reviews' || location.pathname === '/reviews' ? s.active : null}>Отзывы</Link>
                 </li> 
                 <li className={s.li}>
-                    <Link to={'/ru/partners'} className={location.pathname === '/ru/partners' ? s.active : null}>Партнерам</Link>
+                    <Link to={'/ru/partners'} className={location.pathname === '/ru/partners' || location.pathname === '/partners' ? s.active : null}>Партнерам</Link>
                 </li> 
                 <li className={s.li}>
-                    <Link to={'/ru/pages/crypto-helper'} className={location.pathname === '/ru/pages/crypto-helper' ? s.active : null}>Крипто-помощник</Link>
+                    <Link to={'/ru/pages/crypto-helper'} className={location.pathname === '/ru/pages/crypto-helper' || location.pathname === '/pages/crypto-helper' ? s.active : null}>Крипто-помощник</Link>
                 </li> 
             </div>
             
             <li className={cn(s.li, s.last)}>
-                <Link  to={'/ru/support'} className={location.pathname === '/ru/support' ? s.active : null}>Тех.поддержка</Link>
+                <Link  to={'/ru/support'} className={location.pathname === '/ru/support' || location.pathname === '/support' ? s.active : null}>Тех.поддержка</Link>
             </li> 
         </ul>
         </>
